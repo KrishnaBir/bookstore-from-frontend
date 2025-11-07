4fea091a4003093f3ab001bf3633e94038fe0b92
KrishnaBir <jhonhancock99@gmail.com>
2025-11-08 04:34:54 +0530

    global data in navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import Login from "./Login";
+import { useAuth } from "../context/AuthProvider";
 
 function Navbar() {
+  const[authUser, SetAuthUser] = useAuth()
+  console.log(authUser);
+
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
