8f5b04c712a6eeb4ec8e1e90bbd9bf607885b5c7
KrishnaBir <jhonhancock99@gmail.com>
2025-11-08 06:07:52 +0530

    logout button

---
+++ Frontend/src/components/LogOut.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LogOut() {
+  return (
+    <button className='px-3 py-2 bg-red-500 text-white rounded-md cursor-pointer'>logout</button>
+  )
+}
+
+
+export default LogOut
+
