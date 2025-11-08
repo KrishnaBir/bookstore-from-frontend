17927e24109c53bbd2f5d1dd998581221de1973a
KrishnaBir <jhonhancock99@gmail.com>
2025-11-08 09:26:01 +0530

    logout functionality

--- Frontend/src/components/LogOut.jsx
+++ Frontend/src/components/LogOut.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import toast from 'react-hot-toast'
+import { useAuth } from '../context/AuthProvider'
 
 function LogOut() {
-  return (
-    <button className='px-3 py-2 bg-red-500 text-white rounded-md cursor-pointer'>logout</button>
-  )
+    const [authUser, SetAuthUser] = useAuth()
+    const handleLogOut = () => {
+        try {
+            SetAuthUser({
+                ...authUser,
+                user: null
+            })
+            localStorage.removeItem("Users")
+
+            toast.success("Logout successfully")
+        } catch (error) {
+            toast.error("Error : " + error.message)
+        }
+    }
+    return (
+        <button
+            onClick={handleLogOut}
+            className='px-3 py-2 bg-red-500 text-white rounded-md cursor-pointer'>logout</button>
+    )
 }
 
 
